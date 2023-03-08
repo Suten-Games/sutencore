48f4af2ecf94cf315c9500cb71472cd83139b01e
Saxis <alva@suten.io>
2023-03-07 22:39:26 -0700

    UI shell

--- src/game.ts
+++ src/game.ts
@@ -1,6 +1,6 @@
 import resources from "./resources";
-// import { CombatLog } from "./gameUI/combatLog";
-// import { ActionBar } from "./gameUI/actionBar";
+import { CombatLog } from "./gameUI/combatLog";
+import { ActionBar } from "./gameUI/actionBar";
 // import { BackPack } from "./gameUI/backPack";
 import { Khepra } from "./gameUI/khepra";
 
@@ -10,11 +10,12 @@ const apiUrl = local
   ? "http://localhost:8080/player"
   : "https://sutenquestapi.azurewebsites.net/player";
 
-const gameCanvas = new UICanvas();
-// const combatLog = new CombatLog(gameCanvas);
 
-// //SETUP UI
-// const actionBar = new ActionBar(gameCanvas, resources.interface.blueActionBar);
+
+// SETUP UI
+const gameCanvas = new UICanvas();
+const combatLog = new CombatLog(gameCanvas);
+const actionBar = new ActionBar(gameCanvas, resources.interface.blueActionBar);
 // const backPack = new BackPack(gameCanvas, resources.interface.blueBackpack);
 //const khepra = new Khepra(gameCanvas, resources.interface.khepra, backPack);
 const khepra = new Khepra(gameCanvas, resources.interface.khepra);
