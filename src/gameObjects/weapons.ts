217f6280cd08e3c4cbadecb02dd2f41b5e488a7e
Saxis <alva@suten.io>
2024-05-23 07:41:47 -0300

    Fixed weapons

---
+++ src/gameObjects/weapons.ts
@@ -0,0 +1,11 @@
+import resources from "src/resources";
+
+const weaponTypes = {
+    rustySword: { name: "Rusty Sword", texture: resources.loot.rustysword },
+    rustyDagger: { name: "Rusty Dagger", texture: resources.loot.rustydagger },
+    rustyAxe: { name: "Rusty Axe", texture: resources.loot.rustyaxe },
+    crackedStaff: { name: "Cracked Staff", texture: resources.loot.crackedstaff },
+    fineSteelStaff: { name: "Fine Steel Staff", texture: resources.loot.crackedstaff }, // Add new weapon
+    greatStaff: { name: "Great Staff", texture: resources.loot.crackedstaff } // Add new weapon
+    // Add more weapons as needed
+};
